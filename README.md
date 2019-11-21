b167ef1bb195aee26c9d016730dc310fc0384d7c
rami <rami@dhad.me>
2019-11-21 13:44:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Arcade Game
+
+![game Photo](https://d17h27t6h515a5.cloudfront.net/topher/2017/June/5931c951_frogger/frogger.png)
+
+## Introduction
+
+this game built using JavaScript. To run this game,use any web browser such as Google chrome to play.
+
+## how to play
+
+In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down. The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
+### Allow Key press:
+
+![arrow Keys](https://raw.githubusercontent.com/ramimalzahrani/frontend-nanodegree-arcade-game-master/master/images/arrowKeys.png)
+
+- UP key to move up.
+- DOWN key to move down.
+- LEFT key to move left.
+- RIGHT key to move right.
+
+## licenses
+
+The project is part of [Udacity Front End Nano Degree](https://sa.udacity.com/course/front-end-web-developer-nanodegree--nd001).
---
+++ js/app.js
@@ -0,0 +1,104 @@
+// create Enemy class 
+var Enemy = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = "images/enemy-bug.png";
+};
+
+// update will let Enemy move from left to right of canvas
+Enemy.prototype.update = function (dt) {
+    if (this.x <= 505) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = 0;
+    }
+    this.checkCollisions();
+};
+
+// checkCollisions will prevent collision between Enemy and player. if collision happen player will move to start position.
+Enemy.prototype.checkCollisions = function () {
+    if ((((player.x - this.x) < 70) &&
+            ((player.x - this.x) > -70)) &&
+        (((player.y - this.y) < 50) &&
+            (player.y - this.y) > -40)) {
+        alert("Ohh, You Loss!!");
+        player.reset();
+    }
+};
+
+//  render will draw  Enemy
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//create Player class
+var Player = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = "images/char-cat-girl.png";
+};
+
+Player.prototype.update = function (dt) {};
+
+// render will draw Player class
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// reset player position to start position.
+Player.prototype.reset = function () {
+    this.x = 200;
+    this.y = 380;
+    
+};
+// this prototype will move Player when user press up,down,right and left key.
+Player.prototype.handleInput = function (keyup) {
+    if (keyup === 'up') {
+        this.y -= this.speed;
+    } else if (keyup === "down") {
+        this.y += this.speed;
+    } else if (keyup === "right") {
+        this.x += this.speed;
+    } else if (keyup === "left") {
+        this.x -= this.speed;
+    }
+
+    // this if statement will prevent player move out of canvas height and width
+    if (this.x >= 400) {
+        this.x = 400;
+    } else if (this.x <= 0) {
+        this.x = 0;
+    }
+    if (this.y >= 400) {
+        this.y = 400;
+    }
+
+    // if statement that tell that if player rich to water it will alert that he win
+    if (this.y <= 10) {
+        this.reset();
+        alert("Yess, You win!!");
+    }
+
+};
+
+//create Enemy object
+var enemyBug1 = new Enemy(0, 60, Math.floor(Math.random() * 250));
+var enemyBug2 = new Enemy(0, 145, Math.floor(Math.random() * 250));
+var enemyBug3 = new Enemy(0, 230, Math.floor(Math.random() * 250));
+
+// push all Enemy object inside array named allEnemies
+var allEnemies = [enemyBug1, enemyBug2, enemyBug3];
+var player = new Player(200, 380, 80);
+
+// Define the buttons used within the game
+document.addEventListener("keyup", function (e) {
+    var allowedKeys = {
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
